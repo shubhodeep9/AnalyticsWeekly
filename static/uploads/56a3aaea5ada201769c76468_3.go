e7fab4a14388c51d1b93d324df7bac72bccf37bc
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-03-07 20:32:47 +0530

    Minor changes

---
+++ static/uploads/56a3aaea5ada201769c76468_3.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "errors"
+    "github.com/hprose/hprose-go"
+    "net/http"
+)
+
+func hello(name string) string {
+    return "Hello " + name + "!"
+}
+
+type myService struct{}
+
+func (myService) Swap(a int, b int) (int, int) {
+    return b, a
+}
+
+func (myService) Sum(args ...int) (int, error) {
+    if len(args) < 2 {
+        return 0, errors.New("Requires at least two parameters")
+    }
+    a := args[0]
+    for i := 1; i < len(args); i++ {
+        a += args[i]
+    }
+    return a, nil
+}
+
+func main() {
+    service := hprose.NewHttpService()
+    service.AddFunction("hello", hello)
+    service.AddMethods(myService{})
+    http.ListenAndServe(":8080", service)
+}
